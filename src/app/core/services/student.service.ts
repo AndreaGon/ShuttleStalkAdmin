a90b99e59ac5190c0729fb2523d0d53fbe5981a6
Andrea Gonzales <AndreaGon@users.noreply.github.com>
2023-07-24 14:35:17 +0800

    Add student page (WIP)

---
+++ src/app/core/services/student.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { addDoc, collection, deleteDoc, doc, getDocs, query, updateDoc, where } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(
+    private firestore: Firestore
+  ) { }
+
+  collection: any = collection(this.firestore, "students");  
+
+  async getAllStudents(): Promise<any>{
+    const querySnapshot = await getDocs(this.collection);
+
+    let documents: any[] = [];
+    querySnapshot.forEach((doc)=>{
+      documents.push(doc.data());
+    });
+
+    return documents;
+  }
+}
--- src/app/features/pages/students/students.component.ts
+++ src/app/features/pages/students/students.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
+import { StudentService } from 'src/app/core/services/student.service';
 
 @Component({
   selector: 'app-students',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.sass']
 })
 export class StudentsComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'email', 'icnumber', 'program', 'graduation_month', 'graduation_year', 'action'];
+  dataSource?: any;
 
-  constructor() { }
+  listOfStudents: any[] = [];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  constructor(
+    private router: Router,
+    private studentService: StudentService,
+    private changeDetector: ChangeDetectorRef,
+    private spinner: NgxSpinnerService
+  ) { }
 
   ngOnInit(): void {
+    this.refreshStudentTable();
+    this.changeDetector.detectChanges();
+  }
+
+  async refreshStudentTable(): Promise<void>{
+    this.spinner.show();
+    this.listOfStudents = [];
+
+    (await this.studentService.getAllStudents().then((res)=>{
+      res.forEach((doc: any, index: number) =>{
+        this.listOfStudents.push(doc);
+      })
+    }))
+
+    this.dataSource = new MatTableDataSource(this.listOfStudents);
+    this.dataSource.paginator = this.paginator;
+
+    this.spinner.hide();
+  }
+
+  deleteStudent(id: any){
+
   }
 
 }
